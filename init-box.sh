c8670e64e3bc4d78dab478f1e755d5e4d52771f9
Jason Hagglund <hagglundized@gmail.com>
2016-03-23 23:53:02 -0700

    PPA'd git, adding heroku tooling, plus extra bells and whistles

--- init-box.sh
+++ init-box.sh
@@ -5,9 +5,10 @@ set -x
 
 sudo chown -R `whoami` /home/vagrant/dev
 
+sudo add-apt-repository -yy ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get -yy upgrade
-sudo apt-get -yy install git nano build-essential apt-transport-https ca-certificates python-pip
+sudo apt-get -yy install git nano build-essential apt-transport-https ca-certificates python-pip gnupg2 libgmp3-dev
 sudo pip install speedtest-cli
 
 # Install Docker
@@ -23,9 +24,20 @@ sudo usermod -aG docker vagrant
 sudo docker run hello-world
 
 # Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
+# Install heroku toolbelt & docker plugin
+wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install heroku-docker
+
 sudo apt-get -yy autoremove
+
+# git completion, pretty prompt, some aliases
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o /home/vagrant/.git-completion.bash
+cp /vagrant/files/.bash_git_completion /home/vagrant/.bash_git_completion
+cp /vagrant/files/.git_shortcuts /home/vagrant/.git_shortcuts
+echo 'source /home/vagrant/.bash_git_completion' >> /home/vagrant/.bash_profile
+echo 'source /home/vagrant/.git_shortcuts' >> /home/vagrant/.bash_profile
